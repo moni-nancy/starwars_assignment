06105f4287407beaec99751dda69e216572d2868
Nancy <olatujamn.19@student.funaab.edu.ng>
2023-04-19 20:24:25 +0100

    Revert "reset"
    This reverts commit 6e01347b5d56424adbf4a1e0cb976f71c5920210.

---
+++ src/Components/Movie.js
@@ -0,0 +1,18 @@
+import React, {Component } from 'react';
+import MovieCard from './MovieCard';
+import MovieHeader from './MovieHeader'
+import '../styles/app.css';
+
+
+
+
+function Movie() {
+  return (
+    <div className='container'>
+      <MovieHeader/>
+      <MovieCard/>
+    </div>
+  )
+}
+
+export default Movie;
